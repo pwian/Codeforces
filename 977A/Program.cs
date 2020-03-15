0714dc69f85b0dee06e4ddbd3dfff23eafe9591e
pwian <pwian@mail.ru>
2020-03-15 19:41:43 +0300

    Add 977A task

--- 977A/Program.cs
+++ 977A/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Linq;
 
 namespace _977A
 {
     class Program
     {
+        static int[] ReadArray()
+        {
+            return Console.ReadLine()?.Split().Select(int.Parse).ToArray();
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var mas = ReadArray();
+            int n = mas[0], k = mas[1];
+
+            while (n > 1 && k-- > 0)
+            {
+                n = n % 10 == 0 ? n / 10 : n - 1;
+            }
+
+            Console.WriteLine(n);
         }
     }
 }
