87b35e2eb305caee4a3f1b979657f1ac07a04cc1
pwian <pwian@mail.ru>
2020-03-16 19:23:05 +0300

    Add 4A task

---
+++ 4A/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _4A
+{
+    class Program
+    {
+        static int ReadInt()
+        {
+            return int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+        }
+
+        static void Main(string[] args)
+        {
+            var w = ReadInt();
+            var answer = w > 3 && (w - 2) % 2 == 0;
+            Console.WriteLine(answer ? "YES" : "NO");
+        }
+    }
+}
