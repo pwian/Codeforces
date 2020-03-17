2bf89152b656ed1fd3f7013e6d5fd71783304334
pwian <pwian@mail.ru>
2020-03-17 21:15:57 +0300

    add 1A task

---
+++ 1A/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _1A
+{
+    class Program
+    {
+        static int[] ReadArray()
+        {
+            return Console.ReadLine()?.Split().Select(int.Parse).ToArray();
+        }
+
+        static void Main(string[] args)
+        {
+            var mas = ReadArray();
+            long weight = mas[0] % mas[2] == 0 ? mas[0] / mas[2] : mas[0] / mas[2] + 1;
+            var height = mas[1] % mas[2] == 0 ? mas[1] / mas[2] : mas[1] / mas[2] + 1;
+            Console.WriteLine(weight * height);
+        }
+    }
+}
