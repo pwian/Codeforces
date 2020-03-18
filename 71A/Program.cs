d9a4d9a087fc3b4a6114f49a9b3636e171340c8e
pwian <pwian@mail.ru>
2020-03-18 19:40:16 +0300

    Add 71A

---
+++ 71A/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _71A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+            for (int i = 0; i < n; i++)
+            {
+                var str = Console.ReadLine();
+                if (str != null)
+                {
+                    Console.WriteLine(str.Length <= 10 ? str : $"{str[0]}{str.Length - 2}{str[str.Length - 1]}");
+                }
+            }
+        }
+    }
+}
