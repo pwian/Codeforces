333a57d82a71a481c7eebb0d87266de48d04fbf6
pwian <pwian@mail.ru>
2020-03-20 18:06:38 +0300

    Add 339A

---
+++ 339A/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace _339A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join("+", Console.ReadLine()?.Split('+').OrderBy(n => n) ?? throw new InvalidOperationException()));
+        }
+    }
+}
