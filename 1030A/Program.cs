241d70af12217ce2a5b6e16759ae668db9fbfd29
pwian <pwian@mail.ru>
2020-03-21 22:07:08 +0300

    add 1030A

---
+++ 1030A/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _1030A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            Console.WriteLine(Console.ReadLine().Contains("1") ? "HARD" : "EASY");
+        }
+    }
+}
