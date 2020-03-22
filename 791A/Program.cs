921071e9ff81179762eabea5f3658e3dacd3832f
pwian <pwian@mail.ru>
2020-03-22 20:36:13 +0300

    add 791A

---
+++ 791A/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _791A
+{
+    class Program
+    {
+        public static int[] GetIntArray(char inSeparator = ' ')
+        {
+            return Console.ReadLine().Split(inSeparator).Select(int.Parse).ToArray();
+        }
+
+        static void Main(string[] args)
+        {
+            var inputData = GetIntArray();
+            int a = inputData[0], b = inputData[1];
+            //a*3^x > b*2^x
+            //x > log[1.5](b/a)
+            var years = Math.Truncate(Math.Log(b / (double) a, 1.5)) + 1;
+            Console.WriteLine(years);
+        }
+    }
+}
---
+++ ReadWriteData/ReadWrite.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace ReadWriteData
+{
+    public static class ReadWrite
+    {
+        public static int[] GetIntArray(char inSeparator = ' ')
+        {
+            return Console.ReadLine().Split(inSeparator).Select(int.Parse).ToArray();
+        }
+    }
+}
