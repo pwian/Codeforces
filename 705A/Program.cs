6a353a98573804a117d7e7e17f37298d55b366f1
pwian <pwian@mail.ru>
2020-03-23 16:28:21 +0300

    add 705A

---
+++ 705A/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace _705A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var talks = Enumerable.Range(1, n).Select(index => index % 2 == 0 ? "I love" : "I hate");
+            Console.WriteLine(string.Join(" that ", talks) + " it");
+        }
+    }
+}
--- ReadWriteData/ReadWrite.cs
+++ ReadWriteData/ReadWrite.cs
@@ -5,6 +5,11 @@ namespace ReadWriteData
 {
     public static class ReadWrite
     {
+        public static int GetInt()
+        {
+            return int.Parse(Console.ReadLine());
+        }
+
         public static int[] GetIntArray(char inSeparator = ' ')
         {
             return Console.ReadLine().Split(inSeparator).Select(int.Parse).ToArray();
