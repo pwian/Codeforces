9bbe31814125afe2ff22f1d00afbea2733f54450
pwian <pwian@mail.ru>
2020-03-23 19:07:39 +0300

    add 1327

---
+++ 1327A/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace _1327
+{
+    class Program
+    {
+        public static int GetInt()
+        {
+            return int.Parse(Console.ReadLine());
+        }
+
+        public static int[] GetIntArray(char inSeparator = ' ')
+        {
+            return Console.ReadLine().Split(inSeparator).Select(int.Parse).ToArray();
+        }
+
+        public static bool CanSumByOdd(int n, long k)
+        {
+            if (k == 1)
+            {
+                return n % 2 == 1;
+            }
+
+            var sum = (2 + 2 * (k - 1)) * k / 2;
+            return (n - sum >= 0) && (n - sum) % 2 == 0;
+        }
+
+        static void Main(string[] args)
+        {
+            var t = GetInt();
+            for (int i = 0; i < t; i++)
+            {
+                var inputData = GetIntArray();
+                int n = inputData[0], k = inputData[1];
+                Console.WriteLine(CanSumByOdd(n, k) ? "YES" : "NO");
+            }
+        }
+    }
+}
---
+++ 1327B/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1327B
+{
+    class Program
+    {
+        public static int GetInt()
+        {
+            return int.Parse(Console.ReadLine());
+        }
+
+        public static HashSet<int> GetIntExceptFirstArray(char inSeparator = ' ')
+        {
+            var str = Console.ReadLine();
+            if (str.Length == 1)
+            {
+                return new HashSet<int>();
+            }
+            var index = str.IndexOf(' ');
+            str = str.Substring(index + 1);
+            var mas = str.Split(inSeparator).Select(int.Parse);
+            return new HashSet<int>(mas);
+        }
+
+        static void Main(string[] args)
+        {
+            var t = GetInt();
+            for (int i = 0; i < t; i++)
+            {
+                var n = GetInt();
+                var chosenKingdom = new HashSet<int>();
+                int? freePrinces = null;
+                for (int j = 1; j <= n; j++)
+                {
+                    var data = GetIntExceptFirstArray();
+                    var possible = data.FirstOrDefault(king => !chosenKingdom.Contains(king));
+                    if (possible != 0)
+                    {
+                        chosenKingdom.Add(possible);
+                    }
+                    else if (!freePrinces.HasValue)
+                    {
+                        freePrinces = j;
+                    }
+                }
+
+                if (chosenKingdom.Count < n)
+                {
+                    var possible = Enumerable.Range(1, n).First(king => !chosenKingdom.Contains(king));
+                    Console.WriteLine("IMPROVE");
+                    Console.WriteLine($"{freePrinces} {possible}");
+                }
+                else
+                {
+                    Console.WriteLine("OPTIMAL");
+                }
+            }
+        }
+    }
+}
