9f3d6189b8e7da542a595b9132c7c2571fe90e93
pwian <pwian@mail.ru>
2020-03-24 17:40:03 +0300

    add 785A

---
+++ 785A/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _785A
+{
+    class Program
+    {
+        public static int GetInt()
+        {
+            return int.Parse(Console.ReadLine());
+        }
+
+        //      Tetrahedron.Tetrahedron has 4 triangular faces.
+        //      Cube.Cube has 6 square faces.
+        //      Octahedron.Octahedron has 8 triangular faces.
+        //      Dodecahedron.Dodecahedron has 12 pentagonal faces.
+        //      Icosahedron.Icosahedron has 20 triangular faces.
+
+        private static Dictionary<string, int> _faces = new Dictionary<string, int>
+        {
+            { "Tetrahedron", 4},
+            { "Cube", 6},
+            { "Octahedron", 8},
+            { "Dodecahedron", 12},
+            { "Icosahedron", 20},
+        };
+
+        static void Main(string[] args)
+        {
+            var n = GetInt();
+            var sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += _faces[Console.ReadLine()];
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
