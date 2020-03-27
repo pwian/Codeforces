21a2ea49c62a3b01ffd39032a41d13de9184869c
pwian <pwian@mail.ru>
2020-03-27 17:08:38 +0300

    1220A

---
+++ 1220A/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _1220A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            var str = Console.ReadLine();
+            var countOne = str.Count(c => c == 'n');
+            var countZero = str.Count(c => c == 'r');
+            Console.WriteLine(string.Join(" ", Enumerable.Repeat(1, countOne).Concat(Enumerable.Repeat(0, countZero))));
+        }
+    }
+}
