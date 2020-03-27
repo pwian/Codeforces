4fea7f1c275e4afc64005f6edbcd999d55315793
pwian <pwian@mail.ru>
2020-03-27 17:17:02 +0300

    959A

---
+++ 959A/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _959A
+{
+    class Program
+    {
+        public static int GetInt()
+        {
+            return int.Parse(Console.ReadLine());
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetInt() % 2 == 0 ? "Mahmoud" : "Ehab");
+        }
+    }
+}
