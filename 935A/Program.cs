c04de48eb6150d196c00851f9bac215524e6761e
pwian <pwian@mail.ru>
2020-03-27 17:21:14 +0300

    935A

---
+++ 935A/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _935A
+{
+    class Program
+    {
+        public static int GetInt()
+        {
+            return int.Parse(Console.ReadLine());
+        }
+
+        public static int GetCountCase(int n)
+        {
+            var count = 1;
+            for (int i = 2; i <= n / 2; i++)
+            {
+                if (n % i == 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetCountCase(GetInt()));
+        }
+    }
+}
