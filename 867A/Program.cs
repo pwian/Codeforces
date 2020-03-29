d73fa9ff669e42123d91b5fe43ec662a7082bce4
pwian <pwian@mail.ru>
2020-03-29 10:42:42 +0300

    867A

---
+++ 867A/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace _867A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            var str = Console.ReadLine();
+            var sum = Enumerable.Range(1, str.Length - 1).Select(index => str[index] - str[index - 1]).Sum();
+            Console.WriteLine(sum < 0 ? "YES" : "NO");
+        }
+    }
+}
