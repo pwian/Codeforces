7a103e4ff3301b136169de1d9ad2c8c78161a908
pwian <pwian@mail.ru>
2020-03-29 11:20:04 +0300

    1146A

---
+++ 1146A/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace _1146A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var countA = input.Count(symbol => symbol == 'a');
+            Console.WriteLine(2 * countA > input.Length ? input.Length : 2 * countA - 1);
+        }
+    }
+}
