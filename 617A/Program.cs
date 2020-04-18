097e5144dd7387506ae668aa4c62e136ddd2ab20
Aleksandr Ivakin <pwian@mail.ru>
2020-04-18 12:02:07 +0300

    617A

---
+++ 617A/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _617A
+{
+    class Program
+    {
+        public static int GetInt()
+        {
+            return int.Parse(Console.ReadLine());
+        }
+
+        static void Main(string[] args)
+        {
+            var x = GetInt();
+            Console.WriteLine(x % 5 == 0 ? x / 5 : x / 5 + 1);
+        }
+    }
+}
