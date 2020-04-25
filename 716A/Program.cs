4f5866f2ba20b114d1a9da2ff098e9df060bc27c
Aleksandr Ivakin <pwian@mail.ru>
2020-04-25 10:19:34 +0300

    716A

---
+++ 716A/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _716A
+{
+    class Program
+    {
+        public static int[] GetIntArray(char inSeparator = ' ')
+        {
+            return Console.ReadLine().Split(inSeparator).Select(int.Parse).ToArray();
+        }
+
+        static void Main(string[] args)
+        {
+            int[] array = GetIntArray();
+            int n = array[0], c = array[1];
+            array = GetIntArray();
+
+            var lastClearIndex = Enumerable.Range(1, n - 1).FirstOrDefault(index => array[n - index] - array[n - index - 1] > c);
+            if (lastClearIndex == 0)
+            {
+                lastClearIndex = n;
+            }
+            Console.WriteLine(lastClearIndex);
+        }
+    }
+}
