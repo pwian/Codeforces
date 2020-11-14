32cf126db4ec542bde11af151ae79239e824c0e8
Aleksandr Ivakin <pwian@mail.ru>
2020-11-14 18:48:22 +0300

    725A

---
+++ 725A/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace _725A
+{
+    class Program
+    {
+        static int GetNumberInt()
+        {
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        static long GetNumberLong()
+        {
+            return Convert.ToInt64(Console.ReadLine());
+        }
+
+        static int[] GetArrayInt()
+        {
+            string[] @string = Console.ReadLine().Split(' ');
+            int[] array = new int[@string.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(@string[i]);
+            }
+            return array;
+        }
+
+        static long[] GetArrayLong()
+        {
+            string[] @string = Console.ReadLine().Split(' ');
+            long[] array = new long[@string.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt64(@string[i]);
+            }
+            return array;
+        }
+
+        static void Main(string[] args)
+        {
+            int n = GetNumberInt();
+            string str = Console.ReadLine();
+            int count = 0;
+            int i = 0;
+            while (i < str.Length && str[i] == '<')
+            {
+                i++;
+                count++;
+            }
+            int j = str.Length - 1;
+            while (j >= i && str[j] == '>')
+            {
+                j--;
+                count++;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
